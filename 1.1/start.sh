075abc01d6ef2c6f84ed6faf22174b0e16571d1b
gSpot <gspot.x2f@gmail.com>
2024-07-11 04:14:27 +0300

    Fixed

--- 1.1/start.sh
+++ 1.1/start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-WORK_DIR="/opt/ruantiblock_blacklist"
+WORK_DIR="/opt/ruantiblock_blacklist/1.1"
 OUTPUT_DIR="${WORK_DIR}/blacklist-1.1"
 
-${WORK_DIR}/1.1/run_parser.sh ${WORK_DIR}/1.1/ruantiblock_ip.conf ${OUTPUT_DIR}/ip && ${WORK_DIR}/1.1/run_parser.sh ${WORK_DIR}/1.1/ruantiblock_fqdn.conf ${OUTPUT_DIR}/fqdn
+cd ${WORK_DIR}
+${WORK_DIR}/run_parser.sh ${WORK_DIR}/ruantiblock_ip.conf ${OUTPUT_DIR}/ip && ${WORK_DIR}/run_parser.sh ${WORK_DIR}/ruantiblock_fqdn.conf ${OUTPUT_DIR}/fqdn
 
 exit 0
